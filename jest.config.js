41a7bd9e8d6442576edaaa154b997eb88a20eab5
Leomar Nogueira <leomar.nogueira-extern@renault.com>
2023-04-14 14:22:20 -0300

    chore: add jest

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,10 @@
-export default {
+module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
   testEnvironment: 'node',
   transform: {
-    '.+\\.ts$': 'ts-jest'
-  }
-}
+    '.+\\.ts$': 'ts-jest',
+  },
+};
