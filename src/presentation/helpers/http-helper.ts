284c29f9f80b1ea877eaca04dcd6bd6c35cabfd7
Leomar Nogueira <leomar.nogueira-extern@renault.com>
2023-04-15 19:11:38 -0300

    refactor: move server error to a helper method

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,3 +1,4 @@
+import { ServerError } from '../errors/server-error'
 import { type HttpResponse } from '../protocols/http'
 const badRequest = (error: Error): HttpResponse => ({
   statusCode: 400,
@@ -7,7 +8,13 @@ const successRequest = (msg: string): HttpResponse => ({
   statusCode: 200,
   body: msg
 })
+const serverError = (): HttpResponse => ({
+  statusCode: 500,
+  body: new ServerError()
+})
+
 export {
   badRequest,
-  successRequest
+  successRequest,
+  serverError
 }
